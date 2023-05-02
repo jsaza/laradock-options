5aa14b3b0205362de5a9e62cf2d26db8403d8172
Junya Saza <jy.saza@gmail.com>
2023-05-02 09:29:56 +0900

    update nginx/startup.sh - create ssl certificate 5 patterns

--- nginx/startup.sh
+++ nginx/startup.sh
@@ -7,6 +7,22 @@ if [ ! -f /etc/nginx/ssl/default.crt ]; then
     chmod 644 /etc/nginx/ssl/default.key
 fi
 
+# 秘密鍵の作成
+if [ ! -f /etc/nginx/ssl/dev.local.key ]; then
+    openssl genrsa -out "/etc/nginx/ssl/dev.local.key" 2048
+fi
+
+for filename in "dev1.local" "dev2.local" "dev3.local" "dev4.local" "dev5.local"
+do
+    if [ ! -f /etc/nginx/ssl/$filename.crt ]; then
+        # CSRの作成
+        openssl req -new -key "/etc/nginx/ssl/dev.local.key" -out "/etc/nginx/ssl/$filename.csr" -subj "/CN=$filename/O=EnvLocal/C=JP/ST=Fukuoka/L=Fukuoka-City/OU=developer"
+        # SSLサーバー証明書の作成
+        openssl x509 -req -days 3650 -in "/etc/nginx/ssl/$filename.csr" -signkey "/etc/nginx/ssl/dev.local.key" -out "/etc/nginx/ssl/$filename.crt"
+        chmod 644 /etc/nginx/ssl/dev.local.key
+    fi
+done
+
 # Start crond in background
 crond -l 2 -b
 
